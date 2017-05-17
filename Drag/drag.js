03912857f7c17a05057d95c2cdf6ef370ce67aa9
Pira <sritharan.pirathaab@gmail.com>
2017-05-18 00:45:30 +0200

    Drag

---
+++ Drag/drag.js
@@ -0,0 +1,41 @@
+const slider = document.querySelector('.items');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+	
+	isDown = true;
+	slider.classList.add('active');
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+	//console.log(startX);
+	//console.log(scrollLeft);
+});
+
+slider.addEventListener('mouseleave', () => {
+	
+	isDown = false;
+	slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+	
+	isDown = false;
+	slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+	
+	if(!isDown) return;
+
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const move =  (x - startX) * 3;
+
+	slider.scrollLeft = scrollLeft - move;
+
+	//console.log({x, startX});
+});
+
