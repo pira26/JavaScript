560b26449d9521c577a3bffc55be6d93bd0db004
Pira <sritharan.pirathaab@gmail.com>
2017-05-18 20:20:19 +0200

    Sticky Nav

---
+++ Sticky Nav/stickyNav.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector('#main');
+let topOfNav = nav.offsetTop;
+
+const fixNav = () => {
+
+  //console.log(topOfNav, window.scrollY);
+
+  if(window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  }
+  else {
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
+  }
+}
+
+window.addEventListener('scroll', fixNav);
